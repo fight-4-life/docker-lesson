fc13132776d1bad1d4ee12e5397318a8386a012d
fight-4-life <55998000+fight-4-life@users.noreply.github.com>
2020-05-13 20:14:27 +0500

    Change index file

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ require \dirname(__DIR__) . '/vendor/autoload.php';
 
 $_SERVER['APP_DEBUG'] = \filter_var(($_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? null) ?? '0', FILTER_VALIDATE_BOOLEAN);
 $_SERVER['APP_ENV'] = ($_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? null) ?? 'dev';
+$_SERVER['APP_NAME'] = ($_ENV['APP_NAME'] ?? $_SERVER['APP_NAME'] ?? null) ?? 'Vladimir Andreeevich Legotin';
 
 $session = new Session();
 $session->start();
